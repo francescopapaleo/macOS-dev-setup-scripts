0962e0a109c4296e039087a1470bfdaaa9a4d212
francescopapaleo <francesco.papaleo@holophonix.xyz>
2024-09-24 11:36:27 +0200

    packages

--- homebrew/install_packages.sh
+++ homebrew/install_packages.sh
@@ -18,21 +18,21 @@ brew install --cask firefox
 brew install --cask google-drive
 brew install --cask dropbox
 
-brew install --cask slack
+# brew install --cask slack
 brew install --cask zoom
-brew install --cask whatsapp
-brew install --cask telegram
+# brew install --cask whatsapp
+# brew install --cask telegram
 brew install --cask discord
-brew install --cask notion
+# brew install --cask notion
 brew install --cask zotero
-brew install --cask soulseek
+# brew install --cask soulseek
 brew install --cask obs
 
 brew install --cask spotify
 brew install --cask vlc
 brew install --cask freac
 
-brew install --cask sonic-visualiser
+# brew install --cask sonic-visualiser
 brew install --cask reaper
 
 echo "Installation script completed successfully"
